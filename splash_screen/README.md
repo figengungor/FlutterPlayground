a7ad46fa99fac0d2251f1f2be1ef0d57605c78c8
figengungor <figengungor1@gmail.com>
2018-09-17 16:01:44 +0300

    Add splash screen example WIP

---
+++ splash_screen/README.md
@@ -0,0 +1,7 @@
+# splash_screen
+
+A splash screen example.
+
+## Credits
+
+https://flutter.io/assets-and-images/#updating-the-launch-screen
---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Splash Screen Demo'),
+      ),
+    );
+  }
+}
+
+
